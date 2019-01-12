b751c9a72943f6ce31d028bc61c3cba892fde0af
Jakub-Syrek <jakubvonsyrek@gmail.com>
2019-01-12 01:59:31 +0100

    Commit 3

---
+++ Class-Person.ps1
@@ -0,0 +1,13 @@
+﻿class Person {
+    [string] $Name
+    [int] $Age
+
+    Person([string] $Name) {
+        $this.Name = $Name
+    }
+
+    Person([string] $Name, [int]$Age) {
+        $this.Name = $Name
+        $this.Age = $Age
+    }
+}
